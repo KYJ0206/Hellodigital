2138f84f8aa6b31e93fb7b49c024d0475636f357
kyj0206 <gansoo.mayo@gmail.com>
2022-03-23 17:43:28 +0900

    2022-03-23

--- SCRIPT/node/node_ZeroCho/route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.render('main'); // (3)
-});
-router.get('/about', (req, res) => {
-  res.render('about'); // (4)
-});
-module.exports = router;
